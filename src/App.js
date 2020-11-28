3e4a1fead6a14ef26f0d9d24569c42436239304e
yash <hsay0605@gmail.com>
2020-11-28 18:17:35 +0530

    Updated our menu-item withRouter HOC(Higher order Component)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import './App.css';
-import HomePage from './pages/homePage/homepage.jsx'
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/homePage/homepage.jsx";
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
+    // <div>
+    //   <HomePage></HomePage>
+    // </div>
     <div>
-      <HomePage></HomePage>
+      <Switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route path="/shop/hats" component={HatsPage}></Route>
+      </Switch>
     </div>
   );
 }
