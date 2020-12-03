292da0dac75013bf6ab74edd2e81b71db93d642b
yash <hsay0605@gmail.com>
2020-12-03 09:28:38 +0530

    Integrated with Firebase, added the sign in component and added sign in with google

--- src/App.js
+++ src/App.js
@@ -3,28 +3,56 @@ import "./App.css";
 import HomePage from "./pages/homePage/homepage.jsx";
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component.jsx';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import {auth} from './firebase/firebase.utils';
+import React from 'react';
 
-const HatsPage = () => {
-  return (
-    <div>
-      <h1>Hats Page</h1>
-    </div>
-  );
-};
+// const HatsPage = () => {
+//   return (
+//     <div>
+//       <h1>Hats Page</h1>
+//     </div>
+//   );
+// };
 
-function App() {
-  return (
-    // <div>
-    //   <HomePage></HomePage>
-    // </div>
-    <div>
-      <Header></Header>
-      <Switch>
-        <Route exact path="/" component={HomePage}></Route>
-        <Route path="/shop" component={ShopPage}></Route>
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({currentUser: user});
+
+      console.log(user);
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      // <div>
+      //   <HomePage></HomePage>
+      // </div>
+      <div>
+        <Header user={this.state.currentUser}></Header>
+        <Switch>
+          <Route exact path="/" component={HomePage}></Route>
+          <Route path="/shop" component={ShopPage}></Route>
+          <Route path="/signIn" component={SignInAndSignUp}></Route>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from "firebase/app"; //need to access the storage and auth capabilities as the keyword will help us to access it
+import "firebase/firestore"; // for the db storage
+import "firebase/auth"; //for the storage
+
+const config = {
+  apiKey: "AIzaSyA98omIAxnbqPyGARazdP79QE1henMUxfs",
+  authDomain: "boon-db.firebaseapp.com",
+  databaseURL: "https://boon-db.firebaseio.com",
+  projectId: "boon-db",
+  storageBucket: "boon-db.appspot.com",
+  messagingSenderId: "285921592173",
+  appId: "1:285921592173:web:48a29b36395a0577ad9064",
+  measurementId: "G-M9F1CE2878",
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({prompt: 'select_account'});
+export const SignInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
