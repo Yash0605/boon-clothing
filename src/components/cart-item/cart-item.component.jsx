f9df1ad64c1176ce945c264522b054f637868e63
yash <hsay0605@gmail.com>
2021-01-01 09:58:21 +0530

    Added the cart-item component and connected the cart-dropdown to the cart-reducer

---
+++ src/components/cart-item/cart-item.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './cart-item.styles.scss';
+
+const CartItem = ({item : {imageUrl, name, price, quantity}}) => (
+    <div className='cart-item'>
+        <img src={imageUrl} alt='item'></img>
+        <div>
+            <span className='name'>{name}</span>
+            <span> {quantity} * ${price}</span>
+        </div>
+    </div>
+);
+
+export default CartItem;
